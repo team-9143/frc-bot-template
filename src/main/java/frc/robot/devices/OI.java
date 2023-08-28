933904fc0c217e08a064ece93bcf0dcedce36604
Sid Thomas <thomas.siddharth@gmail.com>
2023-08-27 22:26:38 -0700

    [feat] Add mount pose configuration for Pigeon

--- src/main/java/frc/robot/devices/OI.java
+++ src/main/java/frc/robot/devices/OI.java
@@ -12,6 +12,11 @@ public class OI {
   // In proper orientation, Pigeon is flat and facing so that X-axis is forward
   /** Roll increases to the right, pitch to the front, and yaw counter-clockwise. */
   public final static Pigeon2 PIGEON2 = new Pigeon2(DeviceConstants.kPigeonID);
+  static {
+    // Remember to calibrate and update pitch and roll offsets
+    PIGEON2.configMountPose(0, 0, 0);
+    PIGEON2.setYaw(0);
+  }
 
   public final static Limelight LIMELIGHT = new Limelight(NetworkTableInstance.getDefault().getTable("limelight"));
 }
