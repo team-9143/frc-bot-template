22978195c6d107602c2c30feda90770f5966733e
Sid <thomas.siddharth@gmail.com>
2023-12-23 14:06:45 -0800

    Example operator command (#14)
    * [style] Formatting
    
    * [feat] Add to stop method of StateSubsystem
    
    * [feat] Add simple operator state-based command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,10 +3,12 @@ package frc.robot;
 import frc.robot.devices.OI;
 import frc.robot.devices.Controller.btn;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.ExampleStateSubsystem;
+
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 
 /** Robot structure declaration. Initializes trigger mappings, OI devices, and main stop mechanism. */
 public class RobotContainer {
@@ -52,7 +54,16 @@ public class RobotContainer {
 
   private static void configureDriver() {}
 
-  private static void configureOperator() {}
+  private static void configureOperator() {
+    // Button 'A' (hold) sets subsystem to fast
+    final var cFast = new StartEndCommand(
+      () -> ExampleStateSubsystem.getInstance().setState(ExampleStateSubsystem.States.FAST),
+      () -> ExampleStateSubsystem.getInstance().setState(ExampleStateSubsystem.States.STOP),
+      ExampleStateSubsystem.getInstance()
+    );
+    OI.OPERATOR_CONTROLLER.onTrue(btn.A, cFast::schedule);
+    OI.OPERATOR_CONTROLLER.onFalse(btn.A, cFast::cancel);
+  }
 
   /** Stops all motors and disables controllers. Does not stop commands. */
   public static void stop() {
