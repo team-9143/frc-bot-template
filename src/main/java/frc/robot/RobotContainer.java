d379bb8763512121e465ad542e2d806753aa6c21
Sid Thomas <thomas.siddharth@gmail.com>
2023-12-23 12:37:28 -0800

    [feat] Add subsystem stop methods to RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,8 +3,10 @@ package frc.robot;
 import frc.robot.devices.OI;
 import frc.robot.devices.Controller.btn;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-
 import edu.wpi.first.wpilibj2.command.RunCommand;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ExampleStateSubsystem;
 
 /** Robot structure declaration. Initializes trigger mappings, OI devices, and main stop mechanism. */
 public class RobotContainer {
@@ -33,8 +35,9 @@ public class RobotContainer {
     // Button 'B' (hold) will continuously stop all movement
     new Trigger(() -> OI.DRIVER_CONTROLLER.getButton(btn.B) || OI.OPERATOR_CONTROLLER.getButton(btn.B))
       .whileTrue(new RunCommand(
-        RobotContainer::stop
-        // TODO: Add all subsystems as requirements here
+        RobotContainer::stop,
+        ExampleSubsystem.getInstance(),
+        ExampleStateSubsystem.getInstance()
       ) {
         @Override
         public InterruptionBehavior getInterruptionBehavior() {
@@ -53,6 +56,7 @@ public class RobotContainer {
 
   /** Stops all motors and disables controllers. Does not stop commands. */
   public static void stop() {
-    // TODO: Add all subsystem stop methods here
+    ExampleSubsystem.getInstance().stop();
+    ExampleStateSubsystem.getInstance().stop();
   }
 }
