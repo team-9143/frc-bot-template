2221352adff6aadd9028d6cc9fdef37cb214131e
Sid Thomas <thomas.siddharth@gmail.com>
2023-12-24 01:40:58 -0800

    [feat] Add in SwerveDrive class to manage modules

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
+import frc.robot.util.TunableNumber;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 /** Global constants. Should not include functional code. */
 public class Constants {
   public static class DeviceConsts {
@@ -18,5 +23,25 @@ public class Constants {
     public static final double kMaxWheelVelMetersPerSecond = 6 * 0.8; // 80% of theoretical max 6 m/s
     public static final double kMaxTurnVelRadiansPerSecond = 9.5; // 1.5 rotations/sec
     public static final double kMaxModuleRotateSpeedPercentage = 0.65; // Maximum rotational motor speed
+
+    // Upper bound drivetrain accelerations for path following and pose targeting
+    public static final double kMaxLinearAccelMetersPerSecondSquared = kMaxWheelVelMetersPerSecond * 2; // UNIT: meters/s/s
+    public static final double kMaxTurnAccelRadiansPerSecondSquared = kMaxTurnVelRadiansPerSecond * 2; // UNIT: radians/s/s
+
+    // Gains for drivetrain position error -> desired velocity
+    public static final TunableNumber
+      kTranslateP = new TunableNumber("P", 1, "Robot Translation"),
+      kTranslateI = new TunableNumber("I", 0, "Robot Translation"),
+      kTranslateD = new TunableNumber("D", 0, "Robot Translation");
+    public static final TunableNumber
+      kRotateP = new TunableNumber("P", 1, "Robot Rotation"),
+      kRotateI = new TunableNumber("I", 0, "Robot Rotation"),
+      kRotateD = new TunableNumber("D", 0, "Robot Rotation");
+
+    // Drivetrain location control tolerance
+    public static final Pose2d kPosTolerance = new Pose2d(
+      new Translation2d(0.0127, 0.0127), // UNIT: meters
+      Rotation2d.fromDegrees(0.75)
+    );
   }
 }
---
+++ src/main/java/frc/robot/util/SwerveDrive.java
@@ -0,0 +1,264 @@
+package frc.robot.util;
+
+import edu.wpi.first.wpilibj.MotorSafety;
+
+import frc.robot.devices.OI;
+import frc.robot.Constants.DriveConsts;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+
+/** Controls a set of four {@link SwerveModule SwerveModules}. Protected by {@link MotorSafety}, and speeds must be set every iteration. */
+public class SwerveDrive extends MotorSafety {
+  public final SwerveModule[] modules;
+  private Pose2d desiredPose;
+  private SwerveModuleState[] desiredStates = new SwerveModuleState[] {
+    new SwerveModuleState(),
+    new SwerveModuleState(),
+    new SwerveModuleState(),
+    new SwerveModuleState()
+  };
+
+  public final SwerveDriveKinematics kinematics;
+  private final SwerveDrivePoseEstimator odometry;
+
+  /** {@code true} if trajectory following through a {@link HolonomicDriveController}. */
+  private boolean locationControl = false;
+
+  // Create PID controllers for position change -> velocity calculations
+  private static final PIDController x_controller = new PIDController(
+    DriveConsts.kTranslateP.getAsDouble(),
+    DriveConsts.kTranslateI.getAsDouble(),
+    DriveConsts.kTranslateD.getAsDouble()
+  );
+  private static final PIDController y_controller = new PIDController(
+    DriveConsts.kTranslateP.getAsDouble(),
+    DriveConsts.kTranslateI.getAsDouble(),
+    DriveConsts.kTranslateD.getAsDouble()
+  );
+  private static final ProfiledPIDController theta_controller = new ProfiledPIDController(
+    DriveConsts.kRotateP.getAsDouble(),
+    DriveConsts.kRotateI.getAsDouble(),
+    DriveConsts.kRotateD.getAsDouble(),
+    new Constraints(DriveConsts.kMaxTurnVelRadiansPerSecond, DriveConsts.kMaxTurnAccelRadiansPerSecondSquared)
+  );
+
+  // Main controller
+  private static final HolonomicDriveController m_controller = new HolonomicDriveController(x_controller, y_controller, theta_controller);
+
+  // Initialize controller settings and bind Tunables
+  static {
+    m_controller.setTolerance(DriveConsts.kPosTolerance);
+
+    x_controller.setIntegratorRange(-DriveConsts.kMaxWheelVelMetersPerSecond, DriveConsts.kMaxWheelVelMetersPerSecond);
+    y_controller.setIntegratorRange(-DriveConsts.kMaxWheelVelMetersPerSecond, DriveConsts.kMaxWheelVelMetersPerSecond);
+    theta_controller.setIntegratorRange(-DriveConsts.kMaxTurnVelRadiansPerSecond, DriveConsts.kMaxTurnVelRadiansPerSecond);
+
+    DriveConsts.kTranslateP.bindTo(val -> {
+      x_controller.setP(val);
+      y_controller.setP(val);
+    });
+    DriveConsts.kTranslateI.bindTo(val -> {
+      x_controller.setI(val);
+      y_controller.setI(val);
+    });
+    DriveConsts.kTranslateI.bindTo(val -> {
+      x_controller.setI(val);
+      y_controller.setI(val);
+    });
+
+    DriveConsts.kRotateP.bindTo(theta_controller::setP);
+    DriveConsts.kRotateI.bindTo(theta_controller::setI);
+    DriveConsts.kRotateD.bindTo(theta_controller::setD);
+  }
+
+  public SwerveDrive(SwerveModule.SwerveModuleConstants consts_fl, SwerveModule.SwerveModuleConstants consts_fr, SwerveModule.SwerveModuleConstants consts_bl, SwerveModule.SwerveModuleConstants consts_br) {
+    // Turn on motor safety watchdog
+    setSafetyEnabled(true);
+
+    // Initialize swerve modules
+    modules = new SwerveModule[] {
+      new SwerveModule(consts_fl),
+      new SwerveModule(consts_fr),
+      new SwerveModule(consts_bl),
+      new SwerveModule(consts_br)
+    };
+
+    // Initialize kinematics
+    kinematics = new SwerveDriveKinematics(
+      consts_fl.location,
+      consts_fr.location,
+      consts_bl.location,
+      consts_br.location
+    );
+
+    // Initialize odometry - If using vision adjustments, add standard deviation matrices
+    odometry = new SwerveDrivePoseEstimator(
+      kinematics,
+      Rotation2d.fromDegrees(OI.PIGEON2.getYaw()),
+      new SwerveModulePosition[] {
+        new SwerveModulePosition(modules[0].getDistance(), Rotation2d.fromDegrees(modules[0].getAngle())),
+        new SwerveModulePosition(modules[1].getDistance(), Rotation2d.fromDegrees(modules[1].getAngle())),
+        new SwerveModulePosition(modules[2].getDistance(), Rotation2d.fromDegrees(modules[2].getAngle())),
+        new SwerveModulePosition(modules[3].getDistance(), Rotation2d.fromDegrees(modules[3].getAngle()))
+      },
+      new Pose2d() // If using field-relative paths, use field-relative pose here
+    );
+  }
+
+  /** Updates odometry and pushes desired control to modules. Should be called every robot loop. */
+  public void update() {
+    // Update odometry state estimation
+    odometry.update(Rotation2d.fromDegrees(OI.PIGEON2.getYaw()), new SwerveModulePosition[] {
+      new SwerveModulePosition(modules[0].getDistance(), Rotation2d.fromDegrees(modules[0].getAngle())),
+      new SwerveModulePosition(modules[1].getDistance(), Rotation2d.fromDegrees(modules[1].getAngle())),
+      new SwerveModulePosition(modules[2].getDistance(), Rotation2d.fromDegrees(modules[2].getAngle())),
+      new SwerveModulePosition(modules[3].getDistance(), Rotation2d.fromDegrees(modules[3].getAngle()))
+    });
+
+    // If controlled by a desired location, calculates desired states
+    if (locationControl) {
+      // Relatively confident that the rotation aspect of the trajectory pose is supposed to point from the current pose to the trajectory pose
+      desiredStates = kinematics.toSwerveModuleStates(m_controller.calculate(
+        odometry.getEstimatedPosition(),
+        desiredPose,
+        0,
+        desiredPose.getRotation()
+      ));
+    }
+
+    // Normalize and push angles and speeds to modules
+    SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, DriveConsts.kMaxWheelVelMetersPerSecond);
+    modules[0].desiredStateDrive(desiredStates[0]);
+    modules[1].desiredStateDrive(desiredStates[1]);
+    modules[2].desiredStateDrive(desiredStates[2]);
+    modules[3].desiredStateDrive(desiredStates[3]);
+  }
+
+  /** Sets desired module states, without optimizing. */
+  public void setDesiredStates(SwerveModuleState state_fl, SwerveModuleState state_fr, SwerveModuleState state_bl, SwerveModuleState state_br) {
+    desiredStates[0] = state_fl;
+    desiredStates[1] = state_fr;
+    desiredStates[2] = state_bl;
+    desiredStates[3] = state_br;
+
+    locationControl = false;
+
+    feed();
+  }
+
+  /**
+   * Sets desired module states from field relative velocities.
+   *
+   * @param forward forward speed (UNIT: meters/s)
+   * @param left left speed (UNIT: meters/s)
+   * @param ccw counter-clockwise speed (UNIT: ccw radians/s)
+   */
+  public void setDesiredVelocityFieldRelative(double forward, double left, double ccw) {
+    var states = kinematics.toSwerveModuleStates(ChassisSpeeds.fromFieldRelativeSpeeds(forward, left, ccw, Rotation2d.fromDegrees(OI.PIGEON2.getYaw())));
+    setDesiredStates(states[0], states[1], states[2], states[3]);
+  }
+
+  /**
+   * Sets desired module states from robot relative velocities.
+   *
+   * @param forward forward speed (UNIT: meters/s)
+   * @param left left speed (UNIT: meters/s)
+   * @param ccw counter-clockwise speed (UNIT: ccw radians/s)
+   */
+  public void setDesiredVelocity(double forward, double left, double ccw) {
+    var states = kinematics.toSwerveModuleStates(new ChassisSpeeds(forward, left, ccw));
+    setDesiredStates(states[0], states[1], states[2], states[3]);
+  }
+
+  /**
+   * Sets desired pose and linear velocity, to be controlled with a {@link HolonomicDriveController}.
+   *
+   * @param desiredPose robot pose with the same origin as the odometry
+   * @param desiredLinearVelocityMetersPerSecond desired linear velocity for feedforward
+   */
+  public void setDesiredPose(Pose2d desiredPose, double desiredLinearVelocityMetersPerSecond) {
+    this.desiredPose = desiredPose;
+
+    if (!locationControl) {
+      // Reset controllers if swapping into location control
+      x_controller.reset();
+      y_controller.reset();
+      theta_controller.reset(Math.toRadians(OI.PIGEON2.getYaw()));
+    }
+    locationControl = true;
+
+    feed();
+  }
+
+  /**
+   * Reset the odometry to a given position.
+   *
+   * @param positionMetersCCW robot position (UNIT: meters, ccw native angle)
+   */
+  public void resetPosition(Pose2d positionMetersCCW) {
+    odometry.resetPosition(
+      Rotation2d.fromDegrees(OI.PIGEON2.getYaw()),
+      new SwerveModulePosition[] {
+        new SwerveModulePosition(modules[0].getDistance(), Rotation2d.fromDegrees(modules[0].getAngle())),
+        new SwerveModulePosition(modules[1].getDistance(), Rotation2d.fromDegrees(modules[1].getAngle())),
+        new SwerveModulePosition(modules[2].getDistance(), Rotation2d.fromDegrees(modules[2].getAngle())),
+        new SwerveModulePosition(modules[3].getDistance(), Rotation2d.fromDegrees(modules[3].getAngle()))
+      },
+      positionMetersCCW
+    );
+  }
+
+  /** @return the robot's current estimated location */
+  public Pose2d getPose() {return odometry.getEstimatedPosition();}
+
+  /** @return {@code true} if trajectory following and near desired location */
+  public boolean atReference() {return locationControl && m_controller.atReference();}
+
+  /** @return the drivetrain's desired velocities */
+  public ChassisSpeeds getDesiredSpeeds() {return kinematics.toChassisSpeeds(desiredStates);}
+
+  /** @return the drivetrain's actual velocities, as measured by encoders */
+  public ChassisSpeeds getMeasuredSpeeds() {return kinematics.toChassisSpeeds(getMeasuredStates());}
+
+  /** @return individual desired module states */
+  public SwerveModuleState[] getDesiredStates() {return desiredStates;}
+
+  /** @return measured module states */
+  public SwerveModuleState[] getMeasuredStates() {
+    return new SwerveModuleState[] {
+      new SwerveModuleState(modules[0].getVelocity(), Rotation2d.fromDegrees(modules[0].getAngle())),
+      new SwerveModuleState(modules[1].getVelocity(), Rotation2d.fromDegrees(modules[1].getAngle())),
+      new SwerveModuleState(modules[2].getVelocity(), Rotation2d.fromDegrees(modules[2].getAngle())),
+      new SwerveModuleState(modules[3].getVelocity(), Rotation2d.fromDegrees(modules[3].getAngle()))
+    };
+  }
+
+  /** Stop the modules and reset the desired states. */
+  @Override
+  public void stopMotor() {
+    for (SwerveModule module : modules) {module.stopMotor();}
+
+    setDesiredStates(
+      new SwerveModuleState(0, Rotation2d.fromDegrees(modules[0].getAngle())),
+      new SwerveModuleState(0, Rotation2d.fromDegrees(modules[1].getAngle())),
+      new SwerveModuleState(0, Rotation2d.fromDegrees(modules[2].getAngle())),
+      new SwerveModuleState(0, Rotation2d.fromDegrees(modules[3].getAngle()))
+    );
+  }
+
+  @Override
+  public String getDescription() {
+    return "Swerve Drive";
+  }
+}
