64c32e3e9dada6fe3db65e796998fc991b54c390
Sid Thomas <thomas.siddharth@gmail.com>
2023-12-26 22:01:52 -0800

    [style] Rename swerve module logging path

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -72,25 +72,25 @@ public class Constants {
 
     public static final SwerveModuleConstants
       kSwerve_fl = new SwerveModuleConstants(
-        "/modules/FL/", 41, 42, 43, 31.465,
+        "/swerve-modules/front-left/", 41, 42, 43, 31.465,
         new Translation2d(0.22225, 0.22225),
         new PIDController(kDriveP.getAsDouble(), 0, 0),
         new PIDController(kAngleP.getAsDouble(), 0, kAngleD.getAsDouble())
       ),
       kSwerve_fr = new SwerveModuleConstants(
-        "/modules/FR/", 11, 12, 13, 28.037,
+        "/swerve-modules/front-right/", 11, 12, 13, 28.037,
         new Translation2d(0.22225, -0.22225),
         new PIDController(kDriveP.getAsDouble(), 0, 0),
         new PIDController(kAngleP.getAsDouble(), 0, kAngleD.getAsDouble())
       ),
       kSwerve_bl = new SwerveModuleConstants(
-        "/modules/BL/", 31, 32, 33, 86.748,
+        "/swerve-modules/back-left/", 31, 32, 33, 86.748,
         new Translation2d(-0.22225, 0.22225),
         new PIDController(kDriveP.getAsDouble(), 0, 0),
         new PIDController(kAngleP.getAsDouble(), 0, kAngleD.getAsDouble())
       ),
       kSwerve_br = new SwerveModuleConstants(
-        "/modules/BR/", 21, 22, 23, -96.943,
+        "/swerve-modules/back-right/", 21, 22, 23, -96.943,
         new Translation2d(-0.22225, -0.22225),
         new PIDController(kDriveP.getAsDouble(), 0, 0),
         new PIDController(kAngleP.getAsDouble(), 0, kAngleD.getAsDouble())
