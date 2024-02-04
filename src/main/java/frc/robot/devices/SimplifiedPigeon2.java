bbac859cd7954b0fc059628735dba8533fdf81b9
Sid Thomas <thomas.siddharth@gmail.com>
2024-02-03 16:16:44 -0800

    [feat] Theoretocally working SimplifiedPigeon2 class

--- src/main/java/frc/robot/devices/SimplifiedPigeon2.java
+++ src/main/java/frc/robot/devices/SimplifiedPigeon2.java
@@ -1,17 +1,25 @@
 package frc.robot.devices;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.StatusSignal;
+
+import com.ctre.phoenix6.configs.MountPoseConfigs;
+
+import edu.wpi.first.math.geometry.Rotation3d;
 
 /**
- * This class is meant to remove much of the functionality of the Pigeon2 so that data cannot be set by unauthorized code. If new data needs to be available, wrapper methods may be added easily. This class does not implement the Loggable interface because IMU logging should be done by the subsystem it is associated with (usually, the drivetrain). Additionally, the constructor is able to clearly set the orientation of the IMU for standardized use across robots.
+ * This class implements configuration and angle measurement functionality of a Pigeon 2. It is meant mainly to be used to stop unauthorized resetting of this data, as it should only be changed by the odometry.
  * <p> Roll should increase to robot-right, pitch to robot-front, and yaw counter-clockwise. </p>
 */
 public class SimplifiedPigeon2 {
-  private final Pigeon2 PIGEON2;
+  private final StatusSignal<Double> yawSignal;
+  private final StatusSignal<Double> pitchSignal;
+  private final StatusSignal<Double> rollSignal;
 
-  private final boolean invertPitch;
-  private final boolean invertRoll;
+  private final Pigeon2 pigeon2;
 
+  private final double invertPitch;
+  private final double invertRoll;
   private final boolean swapPitchAndRoll;
 
   /**
@@ -22,18 +30,34 @@ public class SimplifiedPigeon2 {
    * @param rollOffset Additive roll offset, best found by calibrating through PhoenixTuner
    * @param invertPitch Whether to invert the pitch to reach the proper positive direction (robot-front)
    * @param invertRoll Whether to invert the roll to reach the proper positive direction (robot-right)
-   * @param swapPitchAndRoll Whether to swap the pitch (should be forward/backward) and the roll (should be left/right) to align the axes
+   * @param swapPitchAndRoll Whether to swap the pitch (should be forward/backward) and the roll (should be right/left) to align the axes
    */
   // TODO: Fix with new ctre lib and move object scope to drivetrain subsystem
-  protected SimplifiedPigeon2(int deviceNumber, double pitchOffset, double rollOffset, boolean invertPitch, boolean invertRoll, boolean swapPitchAndRoll) {
-    PIGEON2 = new Pigeon2(deviceNumber);
-    this.invertPitch = invertPitch;
-    this.invertRoll = invertRoll;
+  public SimplifiedPigeon2(int deviceNumber, double pitchOffset, double rollOffset, boolean invertPitch, boolean invertRoll, boolean swapPitchAndRoll) {
+    pigeon2 = new Pigeon2(deviceNumber);
+    this.invertPitch = invertPitch ? -1 : 1;
+    this.invertRoll = invertRoll ? -1 : 1;
     this.swapPitchAndRoll = swapPitchAndRoll;
 
     // Set up the mount pose and reset the yaw
-    //PIGEON2.configMountPose(0, pitchOffset, rollOffset); HMMMMM
-    PIGEON2.setYaw(0);
+    pigeon2.getConfigurator().apply(new MountPoseConfigs()
+      .withMountPoseYaw(0)
+      .withMountPosePitch(pitchOffset)
+      .withMountPoseRoll(rollOffset)
+    );
+
+    // Configure status signals
+    yawSignal = pigeon2.getYaw();
+    yawSignal.setUpdateFrequency(50);
+
+    pitchSignal = pigeon2.getPitch();
+    pitchSignal.setUpdateFrequency(50);
+
+    rollSignal = pigeon2.getRoll();
+    rollSignal.setUpdateFrequency(50);
+
+    // Remove all other automatic updates
+    pigeon2.optimizeBusUtilization();
   }
 
   /**
@@ -43,28 +67,33 @@ public class SimplifiedPigeon2 {
    * @param pitchOffset Additive pitch offset, best found by calibrating through PhoenixTuner
    * @param rollOffset Additive roll offset, best found by calibrating through PhoenixTuner
    */
-  protected SimplifiedPigeon2(int deviceNumber, double pitchOffset, double rollOffset) {
+  public SimplifiedPigeon2(int deviceNumber, double pitchOffset, double rollOffset) {
     this(deviceNumber, pitchOffset, rollOffset, false, false, false);
   }
 
   /** @return IMU yaw, counter-clockwise positive. If this IMU is being used for odometry, use the odometry heading instead of this method to avoid conflicting data due to odometry pose resets */
   public double getYaw() {
-    return PIGEON2.getYaw().getValueAsDouble();
+    return yawSignal.getValue();
   }
 
   /** @return IMU pitch, robot-front positive */
   public double getPitch() {
     if (swapPitchAndRoll) {
-      return invertPitch ? PIGEON2.getRoll().getValueAsDouble() * -1 : PIGEON2.getRoll().getValueAsDouble(); // Invert the role if needed
+      return rollSignal.getValue() * invertPitch; // Invert the role if needed
     }
-    return invertPitch ? PIGEON2.getPitch().getValueAsDouble() * -1 : PIGEON2.getPitch().getValueAsDouble();
+    return pitchSignal.getValue() * invertPitch;
   }
 
   /** @return IMU roll, robot-right positive */
   public double getRoll() {
     if (swapPitchAndRoll) {
-      return invertRoll ? PIGEON2.getPitch().getValueAsDouble() * -1 : PIGEON2.getPitch().getValueAsDouble();
+      return pitchSignal.getValue() * invertRoll;
     }
-    return invertRoll ? PIGEON2.getRoll().getValueAsDouble() * -1 : PIGEON2.getRoll().getValueAsDouble();
+    return rollSignal.getValue() * invertRoll;
+  }
+
+  /** @return IMU orientation as a {@link Rotation3d}. May cause mild performance issues */
+  public Rotation3d getRotation3d() {
+    return pigeon2.getRotation3d();
   }
 }
